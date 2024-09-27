766d45904af60e21d4808e2eb587f8765f17ff2d
2amkoding <2amkoding@gmail.com>
2024-09-27 16:05:36 -0500

    added sparkles

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,5 +1,5 @@
 import {Canvas, useFrame} from "@react-three/fiber";
-import {OrbitControls} from "@react-three/drei";
+import {OrbitControls, Sparkles} from "@react-three/drei";
 import {useRef} from "react";
 
 const RotatingCube = () => {
@@ -15,6 +15,8 @@ const RotatingCube = () => {
         <mesh ref={meshRef}>
             <cylinderGeometry args={[1, 1, 1]} />
             <meshLambertMaterial color="#468585" emissive="#468585" />
+
+            <Sparkles count ={100} scale={1} size={6} speed={0.2} noise={0.2} />
         </mesh>
     )
 }
